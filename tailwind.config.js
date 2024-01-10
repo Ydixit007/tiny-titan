957b457b0db8902f5f0a4d13b0a3a5bf89b05233
Yash Dixit <ydixit007@gmail.com>
2024-01-10 22:42:42 +0530

    Added Color Theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,21 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      primary: "#242426",
+      primaryDark: "#1E1E1E",
+      grey: "#48484A",
+      greyDark: "#636366",
+      lightGrey: "#AEAEB2",
+      lighterGrey: "#D8D8DC",
+      white: "#EBEBF0",
+      color1: "#0A84FF",
+      color2: "#BF5AF2",
+      color3: "#FF9F0B",
+      color4: "#30D158",
+      color5: "#FF375F",
+      color6: "#AC8E68",
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
