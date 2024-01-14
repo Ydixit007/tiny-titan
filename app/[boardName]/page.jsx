48a14856540c563e5b877b8238271ec99434d5de
Yash Dixit <ydixit007@gmail.com>
2024-01-14 17:31:39 +0530

    Added content on card screen

--- app/[boardName]/page.jsx
+++ app/[boardName]/page.jsx
@@ -19,9 +19,18 @@ export default function page({ params }) {
         initial={{ opacity: 0, x: -50 }}
         animate={{ opacity: 1, x: 0 }}
         exit={{ opacity: 0, x: -50 }}
-        transition={{delay: 0.1}}
+        transition={{ delay: 0.1 }}
+        className="w-full h-screen flex justify-start items-start"
       >
-        Hi , From {params.boardName}
+        <div className="heading">
+          <h1 className="text-lighterGrey font-4xl font-semibold">
+            Tiny-Titan
+          </h1>
+          <p className="text-grey font-md">Empowers you to achieve big things with small steps.</p>
+        </div>
+        <div className="cards-container">
+          
+        </div>
       </motion.main>
     </AnimatePresence>
   );
---
+++ components/TodoCard.jsx
@@ -0,0 +1,7 @@
+export default function TodoCard() {
+    return (
+        <div className="todocard">
+            
+        </div>
+    );
+}
